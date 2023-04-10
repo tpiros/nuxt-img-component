e45a61c2df29d2bcddcfd732e93b6a7fb653ccd4
tpiros <2042718+tpiros@users.noreply.github.com>
2023-04-10 14:06:57 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,31 @@
+<script>
+import '@/css/style.css';
+</script>
+<template>
+  <div class="container">
+    <div class="left">
+      <h1>Built in Component</h1>
+      <nuxt-img src="/img/sample.jpg" width="500" format="webp" />
+      <nuxt-img src="/img/sample2.jpg" width="500" format="webp" />
+      <nuxt-img src="/img/sample3.jpg" width="500" format="webp" />
+    </div>
+    <div class="right">
+      <h1>Cloudinary Image</h1>
+      <img
+        width="500"
+        src="https://res.cloudinary.com/tamas-demo/image/upload/w_500,f_webp,q_auto/img-component/sample.jpg"
+        alt="Sample Image"
+      />
+      <img
+        width="500"
+        src="https://res.cloudinary.com/tamas-demo/image/upload/w_500,f_webp,q_auto/img-component/sample2.jpg"
+        alt="Sample Image"
+      />
+      <img
+        width="500"
+        src="https://res.cloudinary.com/tamas-demo/image/upload/w_500,f_webp,q_auto/img-component/sample3.jpg"
+        alt="Sample Image"
+      />
+    </div>
+  </div>
+</template>
